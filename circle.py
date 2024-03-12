e1bb04d3daab2b6a0c16436398bd1e8b8b1560a5
Yutivich TUNUENG <yutivich2005@gmail.com>
2024-03-12 15:58:09 +0700

    Completed task

--- circle.py
+++ circle.py
@@ -8,7 +8,6 @@ class Circle:
         """Initialize a circle with given radius.
         
         :param radius: radius of the circle, may be zero.
-        :raises ValueError: if radius is negative.
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -19,6 +18,19 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+        >>> circle0 = Circle(0)
+        >>> circle3 = Circle(3)
+        >>> circle4 = Circle(4)
+        >>> circle5 = circle3.add_area(circle4)
+        >>> circle5.get_radius() == 5
+        True
+        >>> circle_negative_rad = Circle(-1)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
+        >>> new_circle = circle3.add_area(circle0)
+        >>> new_circle.get_radius() == circle3.get_radius()
+        True
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
@@ -39,3 +51,7 @@ class Circle:
         return f"Circle({self.radius})"
     
     __repr__ = __str__
+
+
+# if __name__ == "__main__":
+#     c = Circle(-1)
--- test_circle.py
+++ test_circle.py
@@ -8,5 +8,26 @@ Write unit tests as described in README.md.
 
 """
 from circle import Circle
+import unittest
 
-# TODO write 3 tests as described above
+
+class TestCircle(unittest.TestCase):
+
+    def setUp(self):
+        self.circle = Circle(3)
+
+    def test_circle_constructor(self):
+        with self.assertRaises(TypeError):
+            x = Circle("7")
+        with self.assertRaises(ValueError):
+            x = Circle(-1)
+
+    def test_add_area(self):
+        c = Circle(4)
+        new_c = self.circle.add_area(c)
+        self.assertEqual(new_c.get_radius(), 5)
+
+    def test_add_area_radius0(self):
+        c = Circle(0)
+        new_c = self.circle.add_area(c)
+        self.assertEqual(new_c.get_radius(), 3)
