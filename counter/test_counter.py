4072a459ee4f98cda1122461b5afbe7868f0f273
Yutivich TUNUENG <yutivich2005@gmail.com>
2024-03-13 11:50:29 +0700

    Completed task

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,22 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+
+class TestCounter(unittest.TestCase):
+    def setUp(self):
+        self.counter1 = Counter()
+        self.counter2 = Counter()
+
+    def test_singleton(self):
+        self.assertEqual(id(self.counter1), id(self.counter2))
+
+    def test_method(self):
+        self.assertEqual(self.counter1, self.counter2)
+        self.counter1.increment()
+        self.counter2.increment()
+        self.assertEqual(self.counter1.count, self.counter2.count)
+        self.assertEqual(self.counter1.count, 2)
+        self.assertEqual(self.counter2.count, 2)
